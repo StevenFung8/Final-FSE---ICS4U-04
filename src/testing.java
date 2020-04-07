fd0018fea2b7c19e6e82aae4a8202e3afd9cec46
Steven Fung <50207559+StevenFung8@users.noreply.github.com>
2020-04-07 19:32:44 -0400

    playing around (#3)
    Co-authored-by: Steven Fung <32400622@student.publicboard.ca>

--- src/testing.java
+++ src/testing.java
@@ -2,4 +2,7 @@ public class testing {
     public static void main(String[]args){
         System.out.println("gay sex");
     }
+    public static int difference(int num1, int num2){
+        return num1-num2;
+    }
 }
