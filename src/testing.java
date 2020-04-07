43172e979acce1dd4244c1ea8ead9e7624c278de
Steven Fung <32400622@student.publicboard.ca>
2020-04-07 19:37:36 -0400

    playing around

--- src/testing.java
+++ src/testing.java
@@ -1,6 +1,9 @@
 public class testing {
     public static void main(String[]args){
         System.out.println("gay sex");
+        System.out.println("there once was a chinese restaurant");
+        System.out.println("and i bought i loaf of bread");
+        System.out.println("the waiter didn't know my name");
     }
     public static int difference(int num1, int num2){
         return num1-num2;
@@ -8,4 +11,5 @@ public class testing {
     public static double division(double num1 , double num2){
         return num1/num2;
     }
+
 }
