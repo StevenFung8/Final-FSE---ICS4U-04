a54214dd9bedad11952114c621c3ad7d95554d36
Steven Fung <32400622@student.publicboard.ca>
2020-04-16 23:04:16 -0400

    all the word functions have been complete

--- src/BookwormAdventures.java
+++ src/BookwormAdventures.java
@@ -60,7 +60,9 @@ class GamePanel extends JPanel implements KeyListener {
     private Rectangle[] letterSlots = new Rectangle[16];
     private boolean[] letterSlotsCondition = new boolean[16];
     private ArrayList<String> alphabet;
-    private Rectangle resetButton;
+    private ArrayList<String> chosenWords = new ArrayList<String>();
+    private Rectangle resetButton,submitButton;
+    private String selectedWord = "";
     public GamePanel() throws IOException {
         addMouseListener(new clickListener());
         setSize(800,600);
@@ -73,14 +75,49 @@ class GamePanel extends JPanel implements KeyListener {
                 rectCounter++;
             }
         }
-        resetButton = new Rectangle(400,720,480,100);
+        resetButton = new Rectangle(400,720,240,100);
+        submitButton = new Rectangle(650,720,240,100);
         for(int i = 0; i<16;i++){
             letterSlotsCondition[i] = true;
         }
-        alphabet = letters.random16letters();
+        alphabet = letters.randomXletters(16);
 
 
-}
+    }
+    public int letterSlotBoolean(boolean a){
+        int counter = 0;
+        for(boolean l : letterSlotsCondition){
+            if (l == a){
+                counter ++;
+            }
+        }
+        return counter;
+    }
+
+    public void slotReset(){
+        for(int i = 0; i<16;i++){
+            letterSlotsCondition[i] = true;
+        }
+        selectedWord = "";
+        alphabet.removeAll(alphabet);
+        alphabet = letters.randomXletters(16);
+        mx=0;
+        my=0;
+    }
+    public void slotReplace(){
+        int counter = 0;
+        ArrayList <String> replacements = letters.randomXletters(letterSlotBoolean(false));
+        for (int i = 0; i<16;i++){
+            if(!letterSlotsCondition[i]){
+                alphabet.set(i,replacements.get(counter));
+                letterSlotsCondition[i] = true;
+                counter++;
+            }
+        }
+        selectedWord="";
+        mx=0;
+        my=0;
+    }
 
 
     public void addNotify() {
@@ -89,47 +126,73 @@ class GamePanel extends JPanel implements KeyListener {
     }
 
 
-    public void paintComponent(Graphics g){
-        System.out.println(mx + "," + my);
-        int letterSlotCounter = 0;
-        g.setColor(new Color(255,255,255));
-        if (level == 1){
-            g.setColor(new Color(100,100,100));
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(0,0,1280,820);
+        if (level == 1) {
+            g.setColor(Color.white);
         }
         g.setColor(Color.BLACK);
-        g.fillRect(0,240,1280,20);
-        g.fillRect(400,240,10,480);
-        g.fillRect(880,240,10,480);
-        for (String s : alphabet){
-            g.drawImage(Letters.getImage(s),letterSlots[letterSlotCounter].x + 20,letterSlots[letterSlotCounter].y+25,this);
-            letterSlotCounter++;
-        }
-        for (int i = 0; i<16;i++){
-            if(letterSlotsCondition[i]) {
-                g.drawRect(letterSlots[i].x, letterSlots[i].y, letterSlots[i].width, letterSlots[i].height);
+        g.fillRect(0, 240, 1280, 20);
+        g.fillRect(400, 240, 10, 480);
+        g.fillRect(880, 240, 10, 480);
+        for (int i = 0; i < 16; i++) {
+            g.drawRect(letterSlots[i].x, letterSlots[i].y, letterSlots[i].width, letterSlots[i].height);
+            if (letterSlotsCondition[i]) {
+                g.drawImage(Letters.getImage("NORMAL", alphabet.get(i)), letterSlots[i].x + 20, letterSlots[i].y + 25, this);
             }
-            else{
-                g.setColor(Color.WHITE);
-                g.fillRect(letterSlots[i].x, letterSlots[i].y, letterSlots[i].width, letterSlots[i].height);
-            }
+
+
         }
-        for(int i = 0; i<16;i++){
-            if (letterSlots[i].contains(mx,my)){
-                g.setColor(Color.WHITE);
-                g.fillRect(letterSlots[i].x,letterSlots[i].y,letterSlots[i].width,letterSlots[i].height);
+        for (int i = 0; i < 16; i++) {
+            if (letterSlots[i].contains(mx,my)) {
                 letterSlotsCondition[i] = false;
+                if (selectedWord.length() < letterSlotBoolean(false)) {
+                    selectedWord += alphabet.get(i);
+                }
             }
         }
-        g.drawRect(resetButton.x,resetButton.y,resetButton.width+10,resetButton.height);
-        if(resetButton.contains(mx,my)){
-            for(int i = 0; i<16;i++){
+
+
+        g.setColor(Color.BLACK);
+        g.drawRect(resetButton.x, resetButton.y, resetButton.width + 10, resetButton.height);
+        g.drawString("RESET",resetButton.x+resetButton.width/2,resetButton.y+resetButton.height/2);
+        g.drawRect(submitButton.x, submitButton.y, submitButton.width + 10, submitButton.height);
+        g.drawString("SUBMIT WORD",submitButton.x+submitButton.width/2,submitButton.y+submitButton.height/2);
+        if (resetButton.contains(mx, my)) {
+            for (int i = 0; i < 16; i++) {
                 letterSlotsCondition[i] = true;
+                selectedWord = "";
+                g.setColor(Color.WHITE);
+                //g.fillRect(100, 100, 1200, 120);//need to learn how to undraw the letters
             }
         }
-
-
-
-
+        if (selectedWord.length() > 0) {
+            for (int i = 0; i < selectedWord.length(); i++) {
+                g.drawImage(letters.getImage("SMALL", String.valueOf(selectedWord.charAt(i))), 200 + 55 * i, 100, this);
+            }
+        }
+        if (submitButton.contains(mx, my)) {
+            if (letters.checkWord(selectedWord)) {
+                g.setColor(Color.GREEN);
+                g.fillRect(200, 400, 50, 50);
+                chosenWords.add(selectedWord);
+                slotReplace();
+            }
+            if (!letters.checkWord(selectedWord)) {
+                g.setColor(Color.red);
+                g.fillRect(200, 400, 50, 50);
+            }
+        }
+        if (chosenWords.size() >0){
+            g.setFont(new Font("Comic Sans MS", Font.BOLD,20));
+            for (int i = 0; i < chosenWords.size();i++){
+                String word = chosenWords.get(i);
+                String output = word.substring(0,1).toUpperCase() + word.substring(1);
+                g.setColor(Color.BLACK);
+                g.drawString(output,950,300+20*i);
+            }
+        }
     }
 
     @Override
@@ -156,7 +219,6 @@ class GamePanel extends JPanel implements KeyListener {
             my = e.getY();
 
         }
-    }//////////////////////////////My pussy is very flumptiusouss
+    }
 
-}///steven have tiny dick
-
+}
