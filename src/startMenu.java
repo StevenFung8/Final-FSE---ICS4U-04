41d5fa2acdfbc9117dc0e97864c7a6a8ba099f17
dtan3 <dtan3771@gmail.com>
2020-04-28 19:17:50 -0400

    Merge remote-tracking branch 'origin/Steven's-Code' into Steven's-Code
    # Conflicts:
    #	src/BookwormAdventures.java

--- src/startMenu.java
+++ src/startMenu.java
@@ -5,35 +5,53 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class startMenu extends JFrame {
+public class startMenu extends JFrame implements ActionListener{
     private JLayeredPane layeredPane=new JLayeredPane();
+    private JLayeredPane creditsPane=new JLayeredPane();
 
     public startMenu() {
         super("Bookworm Adventures");
-        setSize(714,714);
+        setSize(1280,820);
 
-        ImageIcon backPic = new ImageIcon("Pictures//startBackground.jpg");
+        ImageIcon backPic = new ImageIcon("Pictures/StartMenu/Background.png");
         JLabel back = new JLabel(backPic);
         back.setBounds(0, 0,backPic.getIconWidth(),backPic.getIconHeight());
         layeredPane.add(back,1);
 
-        ImageIcon startPic = new ImageIcon("Pictures//PlayButton.png");
+        ImageIcon startPic = new ImageIcon("Pictures/StartMenu/PlayButton.png");
         JButton startBtn = new JButton(startPic);
-        startBtn.setBackground(Color.BLACK);
         startBtn.setBorder(new LineBorder(Color.BLACK));
         startBtn.addActionListener(new ClickStart());
-        startBtn.setBounds((714/2)-startPic.getIconWidth()/2,(714/2)-startPic.getIconHeight()/2,startPic.getIconWidth(),startPic.getIconHeight());
+        startBtn.setBounds(625-300/2,350,300,100);
         layeredPane.add(startBtn,2);
 
-        ImageIcon randomPic = new ImageIcon("Pictures/bookworm.jpg");
-        JButton creditsBtn = new JButton(randomPic);
-        creditsBtn.setBackground(Color.BLACK);
+        ImageIcon creditsPic = new ImageIcon("Pictures/StartMenu/CreditsButton.png");
+        JButton creditsBtn = new JButton(creditsPic);
         creditsBtn.setBorder(new LineBorder(Color.BLACK));
-        creditsBtn.addActionListener(new ClickStart());
-        creditsBtn.setBounds(50,50,randomPic.getIconWidth(),randomPic.getIconHeight());
-        layeredPane.add(creditsBtn,2);
+        creditsBtn.addActionListener(new CreditStart());
+        creditsBtn.setBounds(625-300/2,500,300,100);
+        layeredPane.add(creditsBtn,3);
+
+
+
+        ImageIcon backPic2 = new ImageIcon("Pictures/StartMenu/Library2.png");
+        JLabel back2 = new JLabel(backPic2);
+        back2.setBounds(0, 0,backPic2.getIconWidth(),backPic2.getIconHeight());
+        creditsPane.add(back2,2);
+
+        ImageIcon backBtnPic = new ImageIcon("Pictures/StartMenu/BackButton.png");
+        JButton backBtn = new JButton(backBtnPic);
+        backBtn.setBorder(new LineBorder(Color.BLACK));
+        backBtn.addActionListener(new backStart());
+        backBtn.setBounds(50,50,300,100);
+        creditsPane.add(backBtn,2);
+
+
+
 
         setContentPane(layeredPane);
+
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setResizable(false);
@@ -41,6 +59,11 @@ public class startMenu extends JFrame {
 
     public static void main(String[] arguments) {
         startMenu frame = new startMenu();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 
     class ClickStart implements ActionListener {
@@ -55,5 +78,20 @@ public class startMenu extends JFrame {
             setVisible(false);
         }
     }
+    class CreditStart implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent evt){
+            setContentPane(creditsPane);
+            setVisible(true);
+        }
+    }
+    class backStart implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent evt){
+            setContentPane(layeredPane);
+            setVisible(true);
+        }
+    }
 }
+
 
